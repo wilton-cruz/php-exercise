0a3557b2bb86e15f7620c1330e98010836d8feed
Wilton cruz <wiltoncruz5@gmail.com>
2017-07-02 16:53:22 -0400

    Add files via upload

---
+++ conditionals.php
@@ -0,0 +1,68 @@
+<?php
+$a = 0;
+$b = 5;
+
+#booleans conditionals Operators...
+if( $a ) {
+  echo 'true';
+}
+
+if( !$a ) {
+  echo 'false';
+}
+
+if( $a == false ) {
+  echo 'false';
+}
+if( $a != true ) {
+  echo 'false';
+}
+
+
+# comparison Operators.
+/*if ($a == $b) {
+  echo ' values are equal ';
+} else {
+ echo ' values are NOT equals'; 
+}
+
+if( ( $a == $b ) == false ){
+echo 'values are NOT equals ';
+} 
+*/
+
+
+/*if( $a != $b ) {
+  echo ' Values are NOT equals ';
+}
+
+if( $a !== $b ) {
+  echo ' NOT Identicals ';
+}*/
+
+
+# comparison statemens...
+
+/*if($a == $b) {
+    echo ' Values are Equals ';
+} elseif ($a < $b) {
+  echo ' $a is less than $b ';
+} elseif ($a > $b) {
+    echo ' $a is greater than $b';
+}
+ else {
+   echo 'values are NOT equals';
+}*/
+
+# second exercise....
+/*$score = 20;
+
+if ($score >= 60) {
+	echo ' you complited the level ';
+} elseif ($score <= 30) {
+	echo ' <h1>you should practice somemore before trying this level again.!! </h1>';
+} 
+else {
+	 echo '<h1> please try again.!!</h1>';
+}*/
+?>
